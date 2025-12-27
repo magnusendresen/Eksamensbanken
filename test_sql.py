5c25eb49b99e27f5675855ba35e86f2ff31e9f02
Magnus Endresen <magnus.stensby.endresen@gmail.com>
2025-12-28 00:04:36 +0100

    Removed old sql orm

--- test_sql.py
+++
@@ -1,5 +0,0 @@
-import sql_icp, os
-
-mydb = sql_icp.DB("eksamensbanken", "postgres", os.getenv("pgsql_pw"))
-
-mydb.add_subject("MAA1", "Matematikk 1T", "Matematikk", ["Algebra", "Funksjoner", "Geometri"])
