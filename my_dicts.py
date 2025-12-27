f557cd5650b7277f905e4c8390d5a3a61681d09e
Magnus Endresen <magnus.stensby.endresen@gmail.com>
2025-12-27 23:59:03 +0100

    Added a script for storing list(s)

---
+++ my_dicts.py
@@ -0,0 +1,14 @@
+MAIN_CATEGORIES = [
+	"Mathematics",
+	"Physics",
+	"Chemistry",
+	"Biology",
+	"Geoscience",
+	"Electronics",
+	"Computing",
+	"Psychology",
+	"Medicine",
+	"Society",
+	"Humanities",
+	"Education"
+]
